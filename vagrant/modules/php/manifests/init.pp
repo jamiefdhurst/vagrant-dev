530eb5f64e16f50891202c30f9d7f050276e16fc
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-06 15:12:44 +0000

    Additional packages for the php + php for nginx.

--- vagrant/modules/php/manifests/init.pp
+++ vagrant/modules/php/manifests/init.pp
@@ -1,5 +1,9 @@
 class php {
-    $packages = ["php5", "php5-cli", "php5-mysql", "php5-dev", "php5-gd", "libapache2-mod-php5", "php5-mcrypt"]
+    $packages = ["php5", "php5-cli", "php5-mysql", "php5-dev", "php5-gd", "libapache2-mod-php5", "php5-mcrypt", "php5-curl"]
+
+    if $service == "ngnix" {
+        $packages.push("php5-fpm") # If the service is nginx let's add php-fastcgi
+    }
 
     package { $packages:
         ensure => present,
