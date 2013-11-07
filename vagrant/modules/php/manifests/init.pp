8347570fdce5d03e9902bedd62e80192e153de1d
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-07 17:58:30 +0000

    Kind of fixed newest php bit...

--- vagrant/modules/php/manifests/init.pp
+++ vagrant/modules/php/manifests/init.pp
@@ -1,25 +1,41 @@
 class php {
-   if $newest_php == 1 {
-    # Add new repo.
-    exec { "add-apt-repository":
-         command => "/usr/bin/add-apt-repository ppa:ondrej/php5"
-    }
-
-     # Update packages
-     exec { 'apt-get update':
-       command => '/usr/bin/apt-get update'
-    }
-   }
 
   $packages = ["php5", "php5-cli", "php5-dev", "php5-gd", "php5-mcrypt", "php5-curl", "php5-fpm"]
 
-  package { $packages:
+  if $newest_php == 1 {
+    # Add new repo.
+    file { "add-repositories php":
+      path    => "/etc/apt/sources.list.d/ondrej-php5.list",
+      ensure  => file,
+      content  => template("php/repo.erb"),
+      notify  => [Service[$service], Exec["apt-get update"]]
+    }
+
+    package { $packages:
+      ensure => latest,
+      require => [
+        Package[$service],
+        File["add-repositories php"]
+      ],
+    }
+
+    service { "php5-fpm":
+      ensure => running,
+      require => Package["php5-fpm"]
+    }
+
+  } else {
+
+    package { $packages:
       ensure => present,
       require => Package[$service]
+    }
+
+    service { "php5-fpm":
+      ensure => running,
+      require => Package["php5-fpm"]
+    }
+
   }
 
-  service { "php5-fpm":
-    ensure => running,
-    require => Package["php5-fpm"]
-  }
 }
