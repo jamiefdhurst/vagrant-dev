dbe443deb67151519d5b3354808b6be1e80e54e7
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-07 09:37:34 +0000

    Run nginx installation after apt-get update

--- vagrant/modules/nginx/manifests/init.pp
+++ vagrant/modules/nginx/manifests/init.pp
@@ -2,8 +2,9 @@ class nginx {
 
   package { "nginx":
     ensure => present,
+    require => Exec["apt-get update"]
   }
- 
+
   service { "nginx":
     ensure => running,
     require => Package["nginx"],
