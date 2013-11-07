28dd5b606ff609a55b9f21cd9dced4b3e2ac65a0
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-07 16:45:48 +0000

    MySQL root Password setting.

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -5,6 +5,7 @@ module VConfig
   $box_ip       = '127.0.0.1' # The internal IP address... Just in case.
   $composer     = 1 # Would you like to use beautiful and magical composer?
   $database     = 'mysql' # Select database software. Please be aware the manifest has to exist... Leave empty to disable.
+  $db_password  = 'root' # What password should be set up for the root user of database?
   $index        = 'index.php' # What file should be read.
   $logs_dir     = '/vagrant/logs' # Server side path to logs.
   $mailcatcher  = 0 # Would you like to install MailCatcher?
--- vagrant/modules/mysql/manifests/init.pp
+++ vagrant/modules/mysql/manifests/init.pp
@@ -5,4 +5,15 @@ class mysql {
     ensure => present,
     require => Package["php5"]
   }
+
+  exec { "mysql-password":
+    require => [
+      Package["mysql-server"],
+      Package["mysql-client"]
+    ],
+    refreshonly => true,
+    unless => "mysqladmin -uroot -p$db_password status",
+    path => "/bin:/usr/bin",
+    command => "mysqladmin -uroot password $db_password",
+  }
 }
