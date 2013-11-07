0656e80b1c513ee746c27c597639da37401485de
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-07 09:44:37 +0000

    php5-fpm addition.

--- vagrant/modules/php/manifests/init.pp
+++ vagrant/modules/php/manifests/init.pp
@@ -1,8 +1,8 @@
 class php {
     $packages = ["php5", "php5-cli", "php5-mysql", "php5-dev", "php5-gd", "libapache2-mod-php5", "php5-mcrypt", "php5-curl"]
 
-    if $service == "ngnix" {
-        $packages.push("php5-fpm") # If the service is nginx let's add php-fastcgi
+    if $service == 'nginx' {
+        $packages += ["php5-fpm"] # If the service is nginx let's add php-fastcgi
     }
 
     package { $packages:
