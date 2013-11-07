cbf2a681270b5d142ce6cd6999f3cbf7d0818860
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-07 17:13:44 +0000

    More mySQL refactoring.

--- vagrant/modules/mysql/manifests/init.pp
+++ vagrant/modules/mysql/manifests/init.pp
@@ -6,14 +6,22 @@ class mysql {
     require => Package["php5"]
   }
 
+  service { "mysqld":
+    ensure => running,
+    require => [
+      Package["mysql-server"],
+      Package["mysql-client"]
+    ],
+  }
+
   exec { "mysql-password":
     require => [
       Package["mysql-server"],
       Package["mysql-client"]
     ],
-    refreshonly => true,
     unless => "mysqladmin -uroot -p$db_password status",
     path => "/bin:/usr/bin",
     command => "mysqladmin -uroot password $db_password",
+    notify => Service["mysqld"]
   }
 }
