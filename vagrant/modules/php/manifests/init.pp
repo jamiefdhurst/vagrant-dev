b0c7e26d47e389b41638633c5c6568f95fe7fb2d
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-07 15:45:23 +0000

    Install newer php?

--- vagrant/modules/php/manifests/init.pp
+++ vagrant/modules/php/manifests/init.pp
@@ -1,10 +1,16 @@
 class php {
-    $packages = ["php5", "php5-cli", "php5-dev", "php5-gd", "php5-mcrypt", "php5-curl", "php5-fpm"]
-
-    package { $packages:
-        ensure => present,
-        require => Package[$service]
+   if $newest_php == 1 {
+    # Add new repo.
+    exec { "add-apt-repository":
+         command => "/usr/bin/add-apt-repository ppa:ondrej/php5"
     }
+
+     # Update packages
+     exec { 'apt-get update':
+       command => '/usr/bin/apt-get update'
+    }
+   }
+
   $packages = ["php5", "php5-cli", "php5-dev", "php5-gd", "php5-mcrypt", "php5-curl", "php5-fpm"]
 
   package { $packages:
