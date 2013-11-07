7245328d421bdb915355805c38ea08594be41b89
Rafal Proszowski <rafal@hurstdev.co.uk>
2013-11-07 12:14:52 +0000

    If no Camel, than Snake...

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -1,14 +1,15 @@
 module VConfig
 
   # Let's have some variables set up front so we can modify them.
-  $boxlink      = 'localhost' # What would be the address you will visit to view your project?
-  $boxip        = '127.0.0.1' # The internal IP address... Just in case.
+  $box_link     = 'localhost' # What would be the address you will visit to view your project?
+  $box_ip       = '127.0.0.1' # The internal IP address... Just in case.
   $database     = 'mysql' # Select database software. Please be aware the manifest has to exist... Leave empty to disable.
   $index        = 'index.php' # What file should be read.
-  $logsdir      = '/vagrant/logs' # Server side path to logs.
+  $logs_dir     = '/vagrant/logs' # Server side path to logs.
+  $mailcatcher  = 0 # Would you like to install MailCatcher?
   $ram          = '512' # Amount of RAM allowed for this machine.
-  $rootdir      = '/pub' # Local side path to the project content.
-  $serveradmin  = 'webmaster@localhost' # E-mail address for the admin. Fake will do...
-  $service      = 'nginx' # nginx|apache
+  $root_dir     = '/pub' # Local side path to the project content.
+  $server_admin = 'webmaster@localhost' # E-mail address for the admin. Fake will do...
+  $service      = 'nginx' # nginx|apache2
 
 end
