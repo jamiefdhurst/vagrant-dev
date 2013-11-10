5079e7024bb5397366e7780467543c7d6f3dd035
Jamie Hurst <jamie@jamiehurst.co.uk>
2013-11-10 18:24:26 +0000

    Changing configuration around to be a bit more logical, and fixing an inconsistency in folder names

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -1,21 +1,29 @@
 module VConfig
 
-  # Let's have some variables set up front so we can modify them.
+  # General configuration
+  $ram          = '512' # Amount of RAM allowed for this machine.
   $box_link     = 'localhost' # What would be the address you will visit to view your project?
   $box_ip       = '127.0.0.1' # The internal IP address... Just in case.
-  $composer     = 1 # Would you like to use beautiful and magical composer?
-  $database     = 'mysql' # Select database software. Please be aware the manifest has to exist... Leave empty to disable.
-  $db_password  = 'root' # What password should be set up for the root user of database?
-  $index        = 'index.php' # What file should be read.
-  $logs_dir     = '/vagrant/logs' # Server side path to logs.
-  $mailcatcher  = 0 # Would you like to install MailCatcher?
-  $memcache     = 1 # Would you like to install and set up Memcache?
-  $newest_php   = 0 # Would you like to install newest PHP version possible or stick with official version from repos?
   $port         = 8080 # Perhaps you fancy different port?
-  $ram          = '512' # Amount of RAM allowed for this machine.
-  $root_dir     = '/pub' # Local side path to the project content.
-  $sendmail     = 1 # Do you wish to include sendmail to your box?
-  $server_admin = 'webmaster@localhost' # E-mail address for the admin. Fake will do...
+
+  # Apache2/Nginx
   $service      = 'nginx' # nginx|apache2
+  $index        = 'index.php' # What file should be read.
+  $server_admin = 'webmaster@localhost' # E-mail address for the admin. Fake will do...
+  $logs_dir     = '/vagrant/logs' # Server side path to logs.
+  $root_dir     = '/vagrant/public' # Local side path to the project content.
+
+  # MySQL
+  $database     = 'mysql' # Select database software, one of mariadb/mysql. Leave empty to disable.
+  $db_password  = 'root' # Which password should be set up for the root user of database?
+
+  # PHP
+  $newest_php   = 0 # Would you like to install newest PHP version possible (5.5) or stick with official version from repos?
+  $composer     = 1 # Would you like to use beautiful and magical composer?
+  $memcache     = 1 # Would you like to install and set up Memcache?
+
+  # Sendmail/mailcatcher
+  $mailcatcher  = 0 # Would you like to install MailCatcher?
+  $sendmail     = 1 # Do you wish to include sendmail to your box?
 
 end
